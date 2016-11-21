0bf00b96ce90789f391a948a22fe19b34055c972
Thomas Wimmer <samo.miw@gmail.com>
2016-11-21 14:32:41 +0100

    Add project

---
+++ Assets/ControllerInput.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerInput : MonoBehaviour {
+
+    public float pickupRadius = 1f;
+    Pickup pickedObject;
+
+    SteamVR_TrackedObject inputDevice;
+    SteamVR_Controller.Device controller
+    {
+        get
+        {
+            return SteamVR_Controller.Input((int)inputDevice.index);
+        }
+    }
+
+    void Start()
+    {
+        inputDevice = GetComponentInChildren<SteamVR_TrackedObject>();
+    }
+
+    void Update()
+    {
+        if (controller.GetPressDown(Valve.VR.EVRButtonId.k_EButton_Grip))
+        {
+            Collider[] pickups = Physics.OverlapSphere(transform.position, pickupRadius);
+            if (pickups != null)
+            {
+                foreach(Collider collider in pickups)
+                {
+                    Pickup pickup = collider.GetComponent<Pickup>();
+                    if (pickup != null)
+                    {
+                        pickup.GetPickedUp(gameObject);
+                        pickedObject = pickup;
+                    }
+                }
+            }
+        }
+
+        if (controller.GetPressUp(Valve.VR.EVRButtonId.k_EButton_Grip))
+        {
+            if (pickedObject != null)
+            {
+                pickedObject.GetReleased(controller.velocity);
+                pickedObject = null;
+            }
+        }
+
+    }
+}
